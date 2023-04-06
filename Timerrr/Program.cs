63c7648908f582073388556ee036a2b8bfd87d4a
Дарья <@LAPTOP-E9K2RNCV>
2023-04-06 20:34:21 +0300

    Добавьте файлы проекта.

---
+++ Timerrr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timerrr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+ {
+            Console.Write("введите часы: "); 
+            int hours = int.Parse(Console.ReadLine()); 
+
+            Console.Write("введите минуты: ");
+            int minutes = int.Parse(Console.ReadLine());
+
+            Console.Write("введите секунды: ");
+            int seconds = int.Parse(Console.ReadLine());
+
+            Timer timer = new Timer(hours, minutes, seconds); 
+            timer.Start(); 
+            Console.ReadKey(); 
+
+        }
+    }
+}
---
+++ Timerrr/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Timer 
+{
+    private int Hours;
+    private int Minutes;
+    private int Seconds; 
+
+    public Timer(int hours, int minutes, int seconds) 
+    {
+        Hours = hours;
+        Minutes = minutes; 
+        Seconds = seconds;
+    }
+
+    public void Start() 
+    {
+        int totalSeconds = Hours * 3600 + Minutes * 60 + Seconds; 
+        
+        for (int i = totalSeconds; i >= 0; i--)  
+        {
+            int h = i / 3600;
+            int m = (i % 3600) / 60;
+            int s = i % 60;
+
+            Console.WriteLine("{0:00}:{1:00}:{2:00}", h, m, s); 
+
+            Thread.Sleep(1000); 
+        }
+
+        Console.WriteLine("таймер завершен"); 
+    }
+}
